b320a68789cbee9277cc00dab60d5f44d351c357
fescii <isfescii@gmail.com>
2023-10-12 17:57:54 +0300

    New app Entry A03

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use actix_web::{App, HttpServer};
 use routes::{auth, user};
 
 fn main() -> std::io::Result<()> {
-    let app = App::new()
-        .service(auth())
-        .service(user());
+	let app = App::new()
+		.service(auth())
+		.service(user());
 
-    HttpServer::new(|| app)
-        .bind("127.0.0.1:8080")?
-        .run()?;
+	HttpServer::new(|| app)
+		.bind("127.0.0.1:8080")?
+		.run()?;
 
-    Ok(())
+	Ok(())
 }
