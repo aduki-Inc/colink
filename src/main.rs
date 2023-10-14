0e93ec2bae4e7170a742a1f3f14ff1f2a60c86be
fescii <isfescii@gmail.com>
2023-10-14 21:09:44 +0300

    Added models: Entry A11

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use actix_web::{web, App, HttpServer};
+use diesel_migrations;
 mod routes;
 mod models;
 mod db;
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  diesel_migrations::run_pending_migrations(&db::connection).unwrap();
+
   HttpServer::new(|| {
     App::new()
       .service(web::resource("/").to(|| async { "hello world" }))
