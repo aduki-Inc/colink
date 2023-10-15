2cb0a4805679122403f676a8ed628b996d34c8f6
fescii <isfescii@gmail.com>
2023-10-15 22:26:12 +0300

    Added models: Register route

--- src/middlewares/auth_middleware.rs
+++ src/middlewares/auth_middleware.rs
@@ -1,20 +1,21 @@
 use crate::db::schema::users::dsl::*;
+use crate::models::users::User;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::result::Error;
 
-pub fn email_exists(other_email: &str, conn: &PgConnection) -> bool {
-    match users.filter(email.eq(other_email)).first::<(i32, String, String, String)>(conn) {
-        Ok(_) => true,
-        Err(Error::NotFound) => false,
-        Err(_) => false,
-    }
+pub fn email_exists(other_email: &str, conn: &mut PgConnection) -> bool {
+  match users.filter(email.eq(other_email)).first::<User>(conn) {
+    Ok(_) => true,
+    Err(Error::NotFound) => false,
+    Err(_) => false,
+  }
 }
 
-pub fn username_exists(username: &str, conn: &PgConnection) -> bool {
-    match users.filter(username.eq(username)).first::<(i32, String, String, String)>(conn) {
-        Ok(_) => true,
-        Err(Error::NotFound) => false,
-        Err(_) => false,
-    }
+pub fn username_exists(other_username: &str, conn: &mut PgConnection) -> bool {
+  match users.filter(username.eq(other_username)).first::<User>(conn) {
+    Ok(_) => true,
+    Err(Error::NotFound) => false,
+    Err(_) => false,
+  }
 }
