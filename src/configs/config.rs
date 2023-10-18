d47ab93722a1f00dc530478b9d9961d257395b19
fescii <isfescii@gmail.com>
2023-10-18 17:47:06 +0300

    Auth JWT: Entry A27

--- src/configs/config.rs
+++ src/configs/config.rs
@@ -1,3 +1,6 @@
+use dotenv::dotenv;
+use std::env;
+
 #[derive(Debug, Clone)]
 pub struct Config {
   pub database_url: String,
@@ -8,10 +11,11 @@ pub struct Config {
 
 impl Config {
   pub fn init() -> Config {
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
-    let jwt_expires_in = std::env::var("JWT_EXPIRED_IN").expect("JWT_EXPIRED_IN must be set");
-    let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
+    let jwt_expires_in = env::var("JWT_EXPIRED_IN").expect("JWT_EXPIRED_IN must be set");
+    let jwt_maxage = env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
 
     Config {
       database_url,
