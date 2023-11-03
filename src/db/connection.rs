e2389e83fa542bcca7538190cbf0ab4c5cdf6979
GabrielGatimu <geraldg652@gmail.com>
2023-11-03 16:18:21 +0300

    Db Connect

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -2,7 +2,7 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use crate::configs::config::Config;
 
-
+// Establish database connection
 pub async fn establish_connection() -> PgConnection{
   let config = Config::init();
 
