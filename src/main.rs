61a0435806f6899155124dafa7bd4820d09309a4
fescii <isfescii@gmail.com>
2024-01-13 18:10:38 +0300

    Handling Payload deserialization error

--- src/main.rs
+++ src/main.rs
@@ -14,33 +14,43 @@ extern crate diesel_derive_enum;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let json_config = web::JsonConfig::default()
-        .limit(4096)
-        .error_handler(|err, _req| {
-            // create custom error response
-            error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
-        });
+	let json_config = web::JsonConfig::default()
+		.limit(4096)
+		.error_handler(|err, _req| {
+			// create custom error response
+			error::InternalError::from_response(err, HttpResponse::BadRequest().json(json!({
+				"success": false,
+				"error_type": "deserialization",
+				"error": "The payload data cannot be validated!",
+			}))).into()
+		});
 
-    HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin("http://localhost:8080")
-            .allowed_methods(vec!["GET", "POST", "PUT"])
-            .allowed_headers(vec![
-                header::CONTENT_TYPE,
-                header::AUTHORIZATION,
-                header::ACCEPT,
-            ])
-            .max_age(3600)
-            .supports_credentials();
+	HttpServer::new(move || {
+		let cors = Cors::default()
+			.allowed_origin("http://localhost:8080")
+			.allowed_methods(vec!["GET", "POST", "PUT"])
+			.allowed_headers(vec![
+				header::CONTENT_TYPE,
+				header::AUTHORIZATION,
+				header::ACCEPT,
+			])
+			.max_age(3600)
+			.supports_credentials();
 
-        App::new()
-            .app_data(json_cfg.clone())
-            .service(routes::auth::auth_config())
-            //			.wrap(handlers::error_handlers::MyErrorHandler)
-            .wrap(cors)
-            .wrap(Logger::default())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+		App::new()
+			.app_data(web::JsonConfig::default()
+				.limit(4096)
+				.error_handler(|err, _req| {
+					// create custom error response
+					error::InternalError::from_response(err, HttpResponse::BadRequest().finish()).into()
+				}),
+			)
+			.service(routes::auth::auth_config())
+			//			.wrap(handlers::error_handlers::MyErrorHandler)
+			.wrap(cors)
+			.wrap(Logger::default())
+	})
+	.bind(("127.0.0.1", 8080))?
+	.run()
+	.await
 }
